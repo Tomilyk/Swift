5829cefcc63278f2e20649f548fcbfd421291623
Tomás <tomilyk@icloud.com>
2021-12-02 21:11:51 -0600

    Segundo commit

---
+++ 02_var_let.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+//let se usa para constantes
+let maximunNumberOfLoginAttemps = 3
+//var se usa para variables
+var currentLoginAttemp = 0
+
+var x = 0.0, y = 0.0, z = 0.0
+
+
+var welcomeMessage : String
+
+welcomeMessage = "¡Hola hdtpm!"
+//Double es un tipo de variable para decimales
+var red, green, blue : Double
+
+//En swift se pueden usar todo tipo de caracteres
+let π = 3.14592
+let ホ : String
+var 😤 = "Enojado"
+
+print(😤)
+
+😤 = "Enojado 2"
+print(😤)
+//Ejemplo de interpolacion
+print("El numero de login actual es: \(currentLoginAttemp) de un total disponible de \(maximunNumberOfLoginAttemps)")
+
+/*Comentario de varias lines
+ aqui continua*/
+
+/*Este es un comentario multilena
+/*Comentario anidado*/
+Esto es parte del primero comentario*/
